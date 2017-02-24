2640f00846a35ad4e23f1ee655b4f333f8e95f52
Adi Ciausu <adrian.ciausu@gmail.com>
2017-02-24 17:53:15 +0200

    setup py work in progress

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'jsonrpc2-base',
+  packages = ['jsonrpc2-base'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A base package for jsp rpc handling',
+  author = 'Bigstep inc',
+  author_email = 'adrian.ciausu@bigstep.com',
+  url = 'https://github.com/bigstepinc/jsonrpc-python',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['jsonrpc2', 'client', 'server', 'base'],
+  classifiers = [],
+)
