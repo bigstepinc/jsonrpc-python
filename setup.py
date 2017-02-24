90a0328c3669080a70a1013b9f3d436aff90a3c8
Adi Ciausu <adrian.ciausu@gmail.com>
2017-02-24 19:24:02 +0200

    fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'jsonrpc2base',
   packages = ['jsonrpc2base'], # this must be the same as the name above
   version = '0.1',
-  description = 'A base package for jsp rpc handling',
+  description = 'JSONRPC 2.0 client over HTTP',
   author = 'Bigstep inc',
   author_email = 'adrian.ciausu@bigstep.com',
   url = 'https://github.com/bigstepinc/jsonrpc-python',
