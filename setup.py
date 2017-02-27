bbfd4b3eeb94a29739a3e6f063910de2d6caaf93
Adi Ciausu <adrian.ciausu@gmail.com>
2017-02-27 15:42:27 +0200

    fixes

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from distutils.core import setup
+
+version = '0.2'
+name = 'jsonrpc2base'
+
 setup(
-  name = 'jsonrpc2base',
-  packages = ['jsonrpc2base'], # this must be the same as the name above
-  version = '0.1',
-  description = 'JSONRPC 2.0 client over HTTP',
-  author = 'Bigstep inc',
-  author_email = 'adrian.ciausu@bigstep.com',
-  url = 'https://github.com/bigstepinc/jsonrpc-python',
-  download_url = 'https://github.com/bigstepinc/jsonrpc-python/tarball/0.1',
-  keywords = ['jsonrpc2', 'client', 'server', 'base'],
-  classifiers = [],
+    name=name,
+    packages=[name],
+    version=version,
+    description='JSONRPC 2.0 client over HTTP',
+    author='Bigstep inc',
+    author_email='adrian.ciausu@bigstep.com',
+    url='https://github.com/bigstepinc/jsonrpc-python',
+    download_url='https://github.com/bigstepinc/jsonrpc-python/tarball/' + version,
+    keywords=['jsonrpc2', 'client', 'server', 'base'],
+    classifiers=[],
 )
